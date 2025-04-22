9ce6635557f801e379472890a11172448997013e
zszz9709 <zszz9709@gmail.com>
2025-04-23 08:52:24 +0900

    feat : farmSectionDto 추가

---
+++ src/main/java/cherrybro/dto/FarmSectionDto.java
@@ -0,0 +1,23 @@
+package cherrybro.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FarmSectionDto {
+	
+	//농장동 고유 번호(PK)
+	private Long farmSectionNo;
+	
+	//농장동 이름(1동, 2동, 3동)
+	private String farmName;
+	
+	//사용자(농장 주인) 번호(FK)
+	private Long usersNo;
+	
+}
