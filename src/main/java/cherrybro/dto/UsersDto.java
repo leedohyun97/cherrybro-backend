8dfece9d2b3bdb1b76c726e330e2d57e5acd3934
zszz9709 <zszz9709@gmail.com>
2025-04-23 08:36:29 +0900

    feat : UsersDto 생성

--- src/main/java/cherrybro/dto/UsersDto.java
+++ src/main/java/cherrybro/dto/UsersDto.java
@@ -1,5 +1,6 @@
 package cherrybro.dto;
 
+import cherrybro.entitiy.role.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class usersDto {
+public class UsersDto {
 	
 	//사용자 고유 번호
 	private Long usersNo;
@@ -17,15 +18,20 @@ public class usersDto {
 	//사용자 이름
 	private String usersName;
 	
+	//사용자 로그인 ID
 	private String usersId;
 	
+	//사용자 로그인 PW
 	private String usersPassword;
 	
+	//사용자 이메일
 	private String usersEmail;
 	
+	//사용자 전화번호
 	private String usersPhone;
 	
-	private Enum usersRole;
+	//사용자 권한(관리자, 농장주)
+	private Role usersRole;
 	
 	
 }
