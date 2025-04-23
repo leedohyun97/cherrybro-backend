b53b81a8e97c591aa7628c829269d3401d14bc13
zszz9709 <zszz9709@gmail.com>
2025-04-23 10:09:05 +0900

    fix : user column 오타 수정

--- src/main/java/cherrybro/entitiy/farm/Farm.java
+++ src/main/java/cherrybro/entitiy/farm/Farm.java
@@ -22,17 +22,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "farm")
+//농장
 public class Farm {
 	
 	@Id//기본 키(PK) 지정
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//MySQL의 AUTO_INCREMENT 사용
 	@Column(name = "farm_no")//실제 DB 컬럼 이름
 	private Long farmNo;//농장 고유 번호(PK)
 	
 	@Column(name = "farm_name")//실제 DB 컬럼 이름
 	private String farmName;//농장 이름
 	
-	@JoinColumn(name = "users_name")//실제 DB 컬럼 이름
+	@JoinColumn(name = "users_no")//실제 DB 컬럼 이름
 	@ManyToOne(fetch = FetchType.LAZY)//N:1 관계 매핑, 지연 로딩 사용
 	private Users users;//사용자 고유 번호(FK)
 	
