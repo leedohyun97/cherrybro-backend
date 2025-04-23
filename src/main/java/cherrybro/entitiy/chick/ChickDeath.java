b4b49afb3321e58da8a2144cb88b953446b602c2
zszz9709 <zszz9709@gmail.com>
2025-04-23 10:54:21 +0900

    feat : ChickEntry(Entity), ChickDeath(Entity) 생성

---
+++ src/main/java/cherrybro/entitiy/chick/ChickDeath.java
@@ -0,0 +1,61 @@
+package cherrybro.entitiy.chick;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import cherrybro.entitiy.farm.Farm;
+import cherrybro.entitiy.farm.FarmSection;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "chick_death")
+//도태폐사
+public class ChickDeath {
+	
+	@Id//기본 키(PK) 지정
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//MySQL의 AUTO_INCREMENT 사용
+	@Column(name = "chick_death")//실제 DB 컬럼 이름
+	private Long chickDeathNo;//도태폐사 고유 번호(PK)
+	
+	@Column(name = "chick_death_number")//실제 DB 컬럼 이름
+	private Integer chickDeathNumber;//도태폐사(마리)
+	
+	@Column(name = "chick_death_date")//실제 DB 컬럼 이름
+	private LocalDate chickDeathDate;//도태폐사 일자
+	
+	@Column(name = "chick_death_create_at")//실제 DB 컬럼 이름
+	private LocalDateTime chickDeathCreateAt;//도태폐사 등록 타임스탬프
+	
+	@JoinColumn(name = "farm_section")//외래 키(FK)로 사용할 컬럼
+	@ManyToOne(fetch = FetchType.LAZY)//N:1 관계 매핑, 지연 로딩 사용
+	private FarmSection farmSection;//농장동 고유 번호(FK)
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/cherrybro/entitiy/chick/ChickEntry.java
@@ -0,0 +1,60 @@
+package cherrybro.entitiy.chick;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import cherrybro.entitiy.farm.FarmSection;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "chick_entry")
+//입추수수
+public class ChickEntry {
+	
+	@Id//기본 키(PK) 지정
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//MySQL의 AUTO_INCREMENT 사용
+	@Column(name = "chick_entry")//실제 DB 컬럼 이름
+	private Long chickEntryNo;//입추수수 고유 번호(PK)
+	
+	@Column(name = "chick_entry_number")//실제 DB 컬럼 이름
+	private Integer chickEntryNumber;//입추수수(마리)
+	
+	@Column(name = "chick_entry_date")//실제 DB 컬럼 이름
+	private LocalDate chickEntryDate;//입추일자
+	
+	@Column(name = "chick_entry_create_at")//실제 DB 컬럼 이름
+	private LocalDateTime chickEntryCreateAt;//입추수수 등록 타임스탬프
+	
+	@JoinColumn(name = "farm_section")//외래 키(FK)로 사용할 컬럼
+	@ManyToOne(fetch = FetchType.LAZY)//N:1 관계 매핑, 지연 로딩 사용
+	private FarmSection farmSection;//농장동 고유 번호(FK)
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
