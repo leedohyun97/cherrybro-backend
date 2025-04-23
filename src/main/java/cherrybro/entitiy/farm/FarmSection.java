ca38d2e5bf578081b71c0ee1948f7df403531758
zszz9709 <zszz9709@gmail.com>
2025-04-23 10:09:18 +0900

    fix : farmSection 생성

---
+++ src/main/java/cherrybro/entitiy/farm/FarmSection.java
@@ -0,0 +1,40 @@
+package cherrybro.entitiy.farm;
+
+import cherrybro.entitiy.users.Users;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "farm_section")
+//농장동
+public class FarmSection {
+	
+	@Id//기본 키(PK) 지정
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//MySQL의 AUTO_INCREMENT 사용
+	@Column(name = "farm_section")//실제 DB 컬럼 이름
+	private Long farmSectionNo;//농장동 고유 번호(PK)
+	
+	@Column(name = "farm_section_name")//실제 DB 컬럼 이름
+	private String farmSectionName;//농장동 이름(1동, 2동, 3동)
+	
+	@JoinColumn(name = "farm_no")//실제 DB 컬럼 이름
+	@ManyToOne(fetch = FetchType.LAZY)//N:1 관계 매핑, 지연 로딩 사용
+	private Farm farm;//농장 고유 번호(FK)
+	
+	
+}
