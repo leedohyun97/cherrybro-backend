e9c8caaaff98bcaf2254c5ad315d8993bf163d87
zszz9709 <zszz9709@gmail.com>
2025-04-28 14:44:43 +0900

    feat : insert SQL 생성

---
+++ src/main/resources/sql/insert.sql
@@ -0,0 +1,30 @@
+INSERT INTO users (users_name, users_id, users_password, users_email, users_phone, users_role)
+VALUES ('김강남', 'GGW', 'GGW123', 'ggw@naver.com', '010-1234-5678', 1);
+
+INSERT INTO farm (farm_name, users_no)
+VALUES ('강남농장', 1);
+
+INSERT INTO farm (farm_name, users_no)
+VALUES ('강북농장', 1);
+
+INSERT INTO farm_section (farm_no, farm_section_name)
+VALUES (1, '1동');
+
+INSERT INTO farm_section (farm_no, farm_section_name)
+VALUES (1, '2동');
+
+INSERT INTO chick_entry (chick_entry_date, chick_entry_number, farm_section)
+VALUES ('2025-04-28', 1000, 1);
+
+INSERT INTO chick_entry (chick_entry_date, chick_entry_number, farm_section)
+VALUES ('2025-04-28', 2000, 2);
+
+INSERT INTO chick_death (chick_death_date, chick_death_number, farm_section)
+VALUES ('2025-04-28', 50, 1);
+
+INSERT INTO chick_death (chick_death_date, chick_death_number, farm_section)
+VALUES ('2025-04-28', 50, 2);
+
+
+
+
