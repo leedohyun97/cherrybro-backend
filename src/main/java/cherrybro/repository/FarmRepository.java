8e9a631e85c72b7b87bd19ddfb70be3efb42486f
zszz9709 <zszz9709@gmail.com>
2025-05-07 07:50:35 +0900

    feat : farmRepository에 사용자 번호로 농장 조회 추가

--- src/main/java/cherrybro/repository/FarmRepository.java
+++ src/main/java/cherrybro/repository/FarmRepository.java
@@ -10,4 +10,7 @@ public interface FarmRepository extends JpaRepository<Farm, Long>{
 	//농장 고유 번호로 농장 조회
 	Optional<Farm> findByFarmNo(Long farmNo);
 	
+	//농장 고유 번호로 농장 조회
+	Optional<Farm> findByUsersNo(Long usersNo);
+	
 }
