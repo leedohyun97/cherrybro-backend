86e44b0680dee1d2b5611382707833fcea175219
zszz9709 <zszz9709@gmail.com>
2025-05-21 13:52:39 +0900

    refactor : 조회, 리스트 조회 요청 시 토큰 포함하여 요청받게 수정

--- src/main/java/cherrybro/controller/FarmRestController.java
+++ src/main/java/cherrybro/controller/FarmRestController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,13 +20,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import cherrybro.auth.PrincipalDetails;
 import cherrybro.dto.FarmDto;
 import cherrybro.response.Response;
 import cherrybro.response.ResponseMessage;
 import cherrybro.response.ResponseStatusCode;
 import cherrybro.service.FarmService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 
 @CrossOrigin//리액트 API 호출 허용
@@ -159,10 +162,15 @@ public class FarmRestController {
 	}
 
 	/* 농장 조회 */
-	@Operation(summary = "농장 조회")
-	@GetMapping("/{usersNo}")
-	public ResponseEntity<Response<FarmDto>> getFarm(@PathVariable("usersNo") Long usersNo) {
+	@Operation(summary = "농장 조회(농장주)")
+	@SecurityRequirement(name = "BearerAuth") // 인증 토큰 필요 명시
+	@PreAuthorize("hasRole('ROLE_FARMER')")
+	@GetMapping("/my-farm")
+	public ResponseEntity<Response<FarmDto>> getFarm(Authentication authentication) {
 		try {
+			//현재 로그인한 사용자의 인증 정보에서 사용자 번호(usersNo) 추출
+			PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+			Long usersNo = principalDetails.getUsersNo();
 			
 			//사용자 번호로 사용자 조회
 			FarmDto farmDto = farmService.findFarmByUsersNo(usersNo);
@@ -197,11 +205,54 @@ public class FarmRestController {
 		}
 	}
 	
+	/* 농장 조회 */
+	@Operation(summary = "농장 조회(관리자)")
+	@SecurityRequirement(name = "BearerAuth") // 인증 토큰 필요 명시
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@GetMapping("/{usersNo}")
+	public ResponseEntity<Response<FarmDto>> getFarm(Authentication authentication, @PathVariable("usersNo") Long usersNo) {
+		try {
+			
+			//사용자 번호로 사용자 조회
+			FarmDto farmDto = farmService.findFarmByUsersNo(usersNo);
+			
+			//응답 객체 생성
+			Response<FarmDto> response = new Response<>();
+			
+			//인코딩 타입 설정
+			HttpHeaders httpHeaders = new HttpHeaders();
+			httpHeaders.setContentType(new MediaType(MediaType.APPLICATION_JSON, Charset.forName("UTF-8")));
+			
+			//응답 객체 설정
+			response.setStatus(ResponseStatusCode.READ_FARM_SUCCESS);
+			response.setMessage(ResponseMessage.READ_FARM_SUCCESS);
+			response.setData(farmDto);
+			
+			//반환할 응답Entity 생성 및 반환
+			return new ResponseEntity<>(response, httpHeaders, HttpStatus.OK);
+			
+		} catch (Exception e) {
+			//에러 로그 출력
+			e.printStackTrace();
+			
+			//에러 응답 객체 반환
+			Response<FarmDto> response = new Response<>();
+			response.setStatus(ResponseStatusCode.READ_FARM_FAIL);
+			response.setMessage(ResponseMessage.READ_FARM_FAIL);
+			response.setData(null);
+			
+			//반환할 응답Entity 생성 및 반환
+			return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
 	
 	/* 농장 리스트 조회 */
-	@Operation(summary = "농장 목록 조회")
-	@GetMapping
-	public ResponseEntity<Response<List<FarmDto>>> getAllFarm() {
+	@Operation(summary = "농장 목록 조회(관리자용)")
+	@SecurityRequirement(name = "BearerAuth") // 인증 토큰 필요 명시
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@GetMapping("/list")
+	public ResponseEntity<Response<List<FarmDto>>> getAllFarm(Authentication authentication) {
 		try {
 			//사용자 번호로 사용자 조회
 			List<FarmDto> farmDtoList = farmService.findFarms();
